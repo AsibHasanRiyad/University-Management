0aa6e061848962d96cd97b2e04d78d44c6bf7905
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-05-16 13:39:19 +0600

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from "./app";
 import config from "./app/config";
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 async function main() {
-  await mongoose.connect(config.dataBaseURL as string);
-  app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`);
-  });
+  try {
+    await mongoose.connect(config.dataBaseURL as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
