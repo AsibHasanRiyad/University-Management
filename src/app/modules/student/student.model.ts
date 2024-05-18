92f5c4c516abac15064da1e201bb3fcd067bdc85
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-05-18 08:17:25 +0600

    capitalie

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -3,7 +3,22 @@ import { Schema, model, connect } from 'mongoose';
 
 // name Schema
 const studentNameSchema = new Schema<Name>({
-  firstName: { type: String, required: [true, 'First name is required'] },
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    maxlength: [20, 'First name can not be more than 20 character'],
+    trim: true,
+    validate: {
+      validator: function (value: String) {
+        if (typeof value !== 'string' || value.length === 0) {
+          return '';
+        }
+        const result = value.charAt(0).toUpperCase() + value.slice(1);
+        return result === value;
+      },
+      message: '{VALUE} is not in capitalize format',
+    },
+  },
   middleName: { type: String },
   lastName: { type: String, required: [true, 'Last name is required'] },
 });
@@ -25,18 +40,22 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String, required: [true, 'Id is required'] },
+  id: { type: String, required: [true, 'Id is required'], unique: true },
   name: {
     type: studentNameSchema,
     required: [true, ' Name Field is required'],
   },
   gender: {
     type: String,
-    enum: ['male', 'female', 'other'],
+    enum: {
+      values: ['male', 'female', 'other'],
+      message:
+        " 'Gender filed can only be the following - 'male ', 'female' or 'other' ",
+    },
     required: [true, 'Gender is required'],
   },
   dateOfBirth: { type: String },
-  email: { type: String, required: [true, 'Email is required'] },
+  email: { type: String, required: [true, 'Email is required'], unique: true },
   emergencyContactNumber: {
     type: Number,
     required: [true, 'Emergency contact is required'],
@@ -44,20 +63,23 @@ const studentSchema = new Schema<Student>({
   profileImage: { type: String },
   bloodGroup: {
     type: String,
-    enum: [
-      'A',
-      'A+',
-      'A-',
-      'B',
-      'B+',
-      'B-',
-      'AB',
-      'AB+',
-      'AB-',
-      'O',
-      'O+',
-      'O-',
-    ],
+    enum: {
+      values: [
+        'A',
+        'A+',
+        'A-',
+        'B',
+        'B+',
+        'B-',
+        'AB',
+        'AB+',
+        'AB-',
+        'O',
+        'O+',
+        'O-',
+      ],
+      message: '{VALUE} is not valid Blood Group',
+    },
   },
   presentAddress: { type: String },
   permanentAddress: { type: String },
