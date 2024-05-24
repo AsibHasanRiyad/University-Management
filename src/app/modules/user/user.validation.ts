2a4abddcc689561e89aa78159abee0d241b5e261
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-05-24 11:43:50 +0600

    zod validation is completed

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,15 +1,13 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
   password: z
-    .string()
+    .string({
+      invalid_type_error: 'Password must be string',
+    })
     .max(20, { message: 'Password can not be more than 20 characters ' })
-    .min(8, { message: 'Password can not be less than 8 characters' }),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['student', 'faculty', 'admin']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
+    .min(8, { message: 'Password can not be less than 8 characters' })
+    .optional(),
 });
 
 export const userValidation = {
