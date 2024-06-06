762d76aa55912095d1d0584dc38b4f7c3c3e36d6
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-06-06 10:25:59 +0600

    zod error handling

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,4 +7,5 @@ export default {
   dataBaseURL: process.env.DATABASE_URL,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASSWORD,
+  node_env: process.env.NODE_ENV,
 };
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,26 +1,44 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
-import { ZodError } from 'zod';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let statusCode = error.statusCode || 500;
   let message = error.message || 'Something went wrong';
-  type TErrorSource = {
-    path: string | number;
-    message: string;
-  }[];
+
   let errorSource: TErrorSource = [
     {
       path: '',
       message: 'Something went wrong',
     },
   ];
+  const handelZodError = (error: ZodError) => {
+    // console.log(error);
+    const errorSource: TErrorSource = error.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    const statusCode = 400;
+    return {
+      statusCode,
+      message: ' Validation Error',
+      errorSource,
+    };
+  };
   if (error instanceof ZodError) {
+    const simplifiedError = handelZodError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
   }
   return res.status(statusCode).json({
     success: false,
     message,
     errorSource,
-    myError: error,
+    stack: config.node_env === 'development' ? error.stack : null,
   });
 };
 export default globalErrorHandler;
