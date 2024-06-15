0568aa824e66abfb627876fa43c91968d9ede5e8
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-06-15 22:18:42 +0600

    validation is added

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,8 +1,40 @@
+import { semesterRegistrationStatus } from './semesterRegistration.constant';
 import { TSemesterRegistration } from './semesterRegistration.interface';
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>(
-  {},
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: semesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 export const semesterRegistrationModel = mongoose.model(
