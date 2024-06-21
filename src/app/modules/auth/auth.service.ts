c4fbd01220ebabe0fad48702079d13ab892ea981
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-06-21 10:59:56 +0600

    comments added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -4,13 +4,13 @@ import { UserModel } from '../user/user.model';
 import { TUserLogin } from './auth.interface';
 
 const loginUser = async (payload: TUserLogin) => {
-  //checking if the user is exist or not
+  //checking if the user is exist or not ?
   const user = UserModel.isUserExistByCustomId(payload.id);
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'User Not Found');
   }
   const isUserExist = await UserModel.findOne({ id: payload?.id });
-  //   check if the user is already deleted
+  //   check if the user is already deleted ?
   if (isUserExist?.isDeleted) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is deleted');
   }
@@ -19,7 +19,7 @@ const loginUser = async (payload: TUserLogin) => {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is Blocked');
   }
 
-  //   check if the password is matched or not
+  //   check if the password is matched or not ?
   if (
     !(await UserModel.isPasswordMatched(
       payload?.password,
