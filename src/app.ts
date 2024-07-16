26a8d0d56729251431ac105800959bff951e6c5d
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-16 15:16:50 +0600

    some changes on env file and app js

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app: Application = express();
 // parser
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  }),
+);
 
 //application route
 app.use('/api/v1', router);
